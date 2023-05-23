ece38c11aab9b85a2006fdc6e2c7f2fb9382a4a2
chklee1048 <52231890+chklee1048@users.noreply.github.com>
2023-05-23 19:10:32 +0900

    first commit

---
+++ src/main/Divide.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Divide {
+    
+    public double divide(double x, double y) {
+        return x/y;
+    }
+}
---
+++ src/main/OperationsMain.java
@@ -0,0 +1,13 @@
+package main;
+
+public class OperationsMain {
+    public static void main(String[] args) {
+        Divide divide = new Divide();
+        int x = 10;
+        int y = 5;
+        
+        // 나눗셈
+        System.out.println(divide.divide(x, y)); 
+        
+    }
+}
